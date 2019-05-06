7f9dfb970d25c4a9f8d80bb6535d75f9d399277b
Marco Yeung <myeung@redhat.com>
2019-05-06 14:15:40 -0400

    add example CRs and fine tune make script
    Signed-off-by: Marco Yeung <myeung@redhat.com>

--- pkg/controller/qdr/qdr_controller.go
+++ pkg/controller/qdr/qdr_controller.go
@@ -219,7 +219,7 @@ func (r *ReconcileQdr) Reconcile(request reconcile.Request) (reconcile.Result, e
 			TransitionTime: metav1.Now(),
 		}
 		instance.Status.Conditions = addCondition(instance.Status.Conditions, condition)
-		r.client.Status().Update(context.TODO(), instance)
+		r.client.Update(context.TODO(), instance)
 	}
 
 	requestCert := configs.SetQdrDefaults(instance)
@@ -435,7 +435,7 @@ func (r *ReconcileQdr) Reconcile(request reconcile.Request) (reconcile.Result, e
 				TransitionTime: metav1.Now(),
 			}
 			instance.Status.Conditions = addCondition(instance.Status.Conditions, condition)
-			r.client.Status().Update(context.TODO(), instance)
+			r.client.Update(context.TODO(), instance)
 			// Deployment created successfully - return and requeue
 			return reconcile.Result{Requeue: true}, nil
 		} else if err != nil {
@@ -462,7 +462,7 @@ func (r *ReconcileQdr) Reconcile(request reconcile.Request) (reconcile.Result, e
 			}
 			instance.Status.Conditions = addCondition(instance.Status.Conditions, condition)
 			instance.Status.PodNames = instance.Status.PodNames[:0]
-			r.client.Status().Update(context.TODO(), instance)
+			r.client.Update(context.TODO(), instance)
 			return reconcile.Result{Requeue: true}, nil
 		}
 	} else if instance.Spec.DeploymentPlan.Placement == v1alpha1.PlacementEvery {
@@ -584,7 +584,7 @@ func (r *ReconcileQdr) Reconcile(request reconcile.Request) (reconcile.Result, e
 	// Update status.PodNames if needed
 	if !reflect.DeepEqual(podNames, instance.Status.PodNames) {
 		instance.Status.PodNames = podNames
-		err := r.client.Status().Update(context.TODO(), instance)
+		err := r.client.Update(context.TODO(), instance)
 		if err != nil {
 			reqLogger.Error(err, "Failed to update pod names")
 			return reconcile.Result{}, err
