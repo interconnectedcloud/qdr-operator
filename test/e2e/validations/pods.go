e207d36695bfa70a8aa3687a42290ba130a3d962
Fernando Giorgetti <fgiorget@redhat.com>
2019-06-21 16:02:50 -0300

    Excluding pods with DeletionTimestamp from validation

--- test/e2e/validations/pods.go
+++ test/e2e/validations/pods.go
@@ -41,47 +41,50 @@ func ValidatePods(cr *v1alpha1.Interconnect, originalReplicaSize string) {
 	count := 0
 	expEnvVars := []string{"APPLICATION_NAME", "QDROUTERD_CONF", "POD_COUNT"}
 	for _, pod := range podList.Items {
-		if strings.HasPrefix(pod.Name, cr.ObjectMeta.Name+"-") {
-			count++
-			if "Running" != pod.Status.Phase {
-				ginkgo.Fail(fmt.Sprintf("Invalid POD Status. Expected: %v. Found: %v",
-					"Running", pod.Status.Phase))
-			}
+		// If pod name does not match expected prefix or pod has been deleted, skip it
+		if !strings.HasPrefix(pod.Name, cr.ObjectMeta.Name+"-") || pod.DeletionTimestamp != nil {
+			continue
+		}
 
-			// Validating QDROUTERD_CONF env var in containers
-			var envVarsFound []string
-			for _, c := range pod.Spec.Containers {
-				for _, envVar := range c.Env {
-					if len(envVar.Value) == 0 {
-						continue
+		count++
+		if "Running" != pod.Status.Phase {
+			ginkgo.Fail(fmt.Sprintf("Invalid POD Status. Expected: %v. Found: %v",
+				"Running", pod.Status.Phase))
+		}
+
+		// Validating QDROUTERD_CONF env var in containers
+		var envVarsFound []string
+		for _, c := range pod.Spec.Containers {
+			for _, envVar := range c.Env {
+				if len(envVar.Value) == 0 {
+					continue
+				}
+				envVarsFound = append(envVarsFound, envVar.Name)
+				switch envVar.Name {
+				case "QDROUTERD_CONF":
+					if !strings.Contains(envVar.Value, "router {") {
+						ginkgo.Fail(fmt.Sprintf("QDROUTERD_CONF does not define the router entity"))
 					}
-					envVarsFound = append(envVarsFound, envVar.Name)
-					switch envVar.Name {
-					case "QDROUTERD_CONF":
-						if !strings.Contains(envVar.Value, "router {") {
-							ginkgo.Fail(fmt.Sprintf("QDROUTERD_CONF does not define the router entity"))
-						}
-						if !strings.Contains(envVar.Value, "listener {") {
-							ginkgo.Fail(fmt.Sprintf("QDROUTERD_CONF does not define any listener"))
-						}
-					case "APPLICATION_NAME":
-						if envVar.Value != cr.ObjectMeta.Name {
-							ginkgo.Fail(fmt.Sprintf("APPLICATION_NAME does not match expected value: %v",
-								cr.ObjectMeta.Name))
-						}
-					case "POD_COUNT":
-						if envVar.Value != originalReplicaSize {
-							ginkgo.Fail(fmt.Sprintf("POD_COUNT does not match expected value: %v. Found: %v",
-								originalReplicaSize, envVar.Value))
-						}
+					if !strings.Contains(envVar.Value, "listener {") {
+						ginkgo.Fail(fmt.Sprintf("QDROUTERD_CONF does not define any listener"))
+					}
+				case "APPLICATION_NAME":
+					if envVar.Value != cr.ObjectMeta.Name {
+						ginkgo.Fail(fmt.Sprintf("APPLICATION_NAME does not match expected value: %v",
+							cr.ObjectMeta.Name))
+					}
+				case "POD_COUNT":
+					if envVar.Value != originalReplicaSize {
+						ginkgo.Fail(fmt.Sprintf("POD_COUNT does not match expected value: %v. Found: %v",
+							originalReplicaSize, envVar.Value))
 					}
 				}
 			}
+		}
 
-			if !utils.ContainsAll(utils.FromStrings(expEnvVars), utils.FromStrings(envVarsFound)) {
-				ginkgo.Fail(fmt.Sprintf("Missing EnvVars in Pod. Expected: %v. Found: %v",
-					expEnvVars, envVarsFound))
-			}
+		if !utils.ContainsAll(utils.FromStrings(expEnvVars), utils.FromStrings(envVarsFound)) {
+			ginkgo.Fail(fmt.Sprintf("Missing EnvVars in Pod. Expected: %v. Found: %v",
+				expEnvVars, envVarsFound))
 		}
 
 	}
